4050472408af8b3695759b870b4e7d2bad7f1369
Trippy Leaf <95711738+TrippyLeaf@users.noreply.github.com>
2022-05-04 16:07:01 -0400

    Update Welcome.js

--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -7,33 +7,27 @@ export default function Welcome() {
       <div className='instructions'>
         <header>Description</header>
         <p>
-          Deposit Crypto such as Bitcoin, Ethereum, Solana or USDC!. 
-          Fast payouts and fair odds.
-          Create your Betcoinz account today!
+         Create your MakeBetz account today then deposit Crypto like Bitcoin, ETH, SOL, USDC
         </p>
         <header>Matches</header>
         <p>
-          All matches will be closed 5 min before 
-          they have started and all bets placed on those matches are settled 10 mins after 
+          Matches are closed 5 min before 
+          the start time and all bets placed on matches are settled shortly after 
           the results have been finalized.
         </p>
         <header>Create your Account!</header>
         <p>
             Users need to register an account in order to place bets. Each new account
-            is eligible for a Deposit Bonus of UP to $250. Once you have created an account, 
-            you can log in and start placing your bets!
+            is eligible for a Deposit Bonus of UP to $250! Once you have created an account, 
+            you can log in, make a deposit and start placing your bets!
         </p>
     
         <p>
           To check the results of your bets, simply click on "Bet History", at the top of the 
           page to the right of your username. There you will find a list of all your previous bet results.
         </p>
-        <header>Reload Balance</header>
-        <p>
-          Blown up your bankroll? Just click the "Deposit" button that will 
-          appear where your balance used to be. Deposit more Crypto and your account will be reloaded in no time.
-        </p>
-        <p>Good Luck and thanks for checking Betcoinz out!</p>
+      
+        <p>Thanks for visiting MakeBetz!</p>
       </div>
     </section>
   );
