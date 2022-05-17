bbdb3571f64fd29524054e2c1659dca530884ca3
Trippy Leaf <95711738+TrippyLeaf@users.noreply.github.com>
2022-05-16 23:51:02 -0400

    update

--- src/components/Navbar/LoginOptions/LoginOptions.js
+++ src/components/Navbar/LoginOptions/LoginOptions.js
@@ -19,6 +19,7 @@ export default function LoginOptions(props) {
     <>
       <div className='login_options'>
         <button className='login_option' onClick={() => setFrom('Login')}>Login</button> 
+        <div className='separator'>|</div>
         <button className='login_option' type='button' onClick={() => setFrom('Register')}>Join Now</button>
       </div>
       <div className='form_container'>
